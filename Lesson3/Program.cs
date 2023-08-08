2cbaf28ffea5388df031b84f0cae4fe2e08cbb8f
d.ryabchuk <d.ryabchuk@pnn.in.ua>
2023-08-08 12:59:14 +0300

    first version

---
+++ Lesson3/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lesson3
+{
+    public class App
+    {
+        public static void Main()
+        {
+            List<int> list = null;
+
+            Console.WriteLine(GetValue(list));
+
+            var myObject = new MyClass();
+
+            if (myObject == null || myObject.myObject == null || myObject.myObject.lastObject == null) return;
+
+        }
+
+        public static int GetValue(List<int> list) => list.Count switch
+        {
+            0 => 0,
+            1 => list[0],
+            2 => list[1],
+            3 => list[2],
+            4 => list[3], 
+            _ => -1
+        };
+    }
+
+    public class MyClass
+    {
+        public MyClass2 myObject
+        {
+            get;
+            set;
+        }
+    }
+
+    public class MyClass2
+    {
+        public MyClass3 lastObject
+        {
+            get;
+            set;
+        }
+    }
+
+    public class MyClass3
+    {
+
+    }
+}
